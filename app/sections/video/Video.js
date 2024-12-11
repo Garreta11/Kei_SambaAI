0122f1d6b208089763f4ea5356027120d3ecbe9f
Jordi Garreta <jordigarreta11@gmail.com>
2024-12-11 18:52:56 +0100

    change background and video timeline

--- app/sections/video/Video.js
+++ app/sections/video/Video.js
@@ -18,52 +18,29 @@ const Video = ({ url, text }) => {
       videoUrl: url,
     });
 
-    const ammountToScroll = 2 * window.innerHeight;
-    const startPixelate = 0.2;
-    const endPixelate = 0.8;
+    const ammountToScroll = 4 * window.innerHeight;
 
-    // ScrollTrigger setup
-    ScrollTrigger.create({
-      trigger: containerRef.current,
-      start: 'top top', // Start when the container is at the top of the viewport
-      end: '+=' + ammountToScroll,
-      scrub: true, // Smooth scrubbing
-      pin: true,
-      onUpdate: (self) => {
-        let progress = self.progress; // Value between 0 and 1
-
-        // Apply a threshold to delay the effect
-        if (progress < startPixelate) {
-          progress = 0; // No effect before the delay threshold
-        } else if (progress < endPixelate) {
-          // Normalize progress after the delay
-          progress = (progress - startPixelate) / (1 - startPixelate);
-        }
-
-        // Handle scale effect when progress is greater than 0.8
-        if (progress >= endPixelate) {
-          const mappedProgress = (1 - progress) / 0.2; // Maps 0.8 to 1 -> 1 to 0
-          const scaleValue = mappedProgress; // Scale decreases linearly
-          containerRef.current.style.transform = `scale(${scaleValue})`;
-          containerRef.current.style.opacity = `${scaleValue}`;
-          containerRef.current.style.filter = `blur(${
-            (1 - scaleValue) * 100
-          }px)`;
-        } else {
-          containerRef.current.style.filter = `blur(0px)`;
-          containerRef.current.style.transform = 'scale(1)';
-        }
-
-        // Modify sphere based on adjusted progress
-        if (outputRef.current.world.plane) {
-          const minPixel = 2; // Minimum value of uPixel
-          const maxPixel = 500; // Maximum value of uPixel
-          const interpolatedValue = minPixel + progress * (maxPixel - minPixel);
-          outputRef.current.world.plane.material.uniforms.uPixel.value =
-            interpolatedValue;
-        }
+    const timeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: containerRef.current,
+        start: 'top top',
+        end: '+=' + ammountToScroll,
+        scrub: true,
+        pin: true,
       },
     });
+
+    timeline.to(outputRef.current.world.plane.material.uniforms.uPixel, {
+      value: 500,
+    });
+
+    timeline.to(containerRef.current, {
+      scale: 0,
+      filter: 'blur(100px)',
+      opacity: 0,
+    });
+
+    return () => ScrollTrigger.getAll().forEach((st) => st.kill());
   }, []);
 
   return (
